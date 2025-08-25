120c0c6b801318e3af37a6218bb8565306b65897
Anas Hamad <109982019+anashamad9@users.noreply.github.com>
2025-08-25 22:56:46 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
-/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-
-  // Needed for GitHub Pages project sites
   basePath: isProd ? '/temp-project' : '',
   assetPrefix: isProd ? '/temp-project/' : '',
-
-  // optional but helps with GH Pages routing of static export
   trailingSlash: true,
-
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
