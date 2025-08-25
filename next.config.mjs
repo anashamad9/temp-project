190f528b7a5a32e706eb3e607473e940c69a6568
Anas Hamad <109982019+anashamad9@users.noreply.github.com>
2025-08-25 23:04:39 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.NODE_ENV === "production";
 
-const nextConfig = {
-  output: 'export',
+export default {
+  output: "export",
   images: { unoptimized: true },
-  basePath: isProd ? '/temp-project' : '',
-  assetPrefix: isProd ? '/temp-project/' : '',
+  basePath: isProd ? "/temp-project" : "",
+  assetPrefix: isProd ? "/temp-project/" : "",
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/temp-project" : "",
+  },
 };
-
-export default nextConfig;
